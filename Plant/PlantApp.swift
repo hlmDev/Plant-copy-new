77f51c5dbed88dc3478e36d9c496070071a39f12
hlmDev <a7m.almu@gmail.com>
2024-10-20 15:53:53 +0300

    Initial Commit

---
+++ Plant/PlantApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantApp.swift
+//  Plant
+//
+//  Created by Ahlam Majed on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlantTests/PlantTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlantTests.swift
+//  PlantTests
+//
+//  Created by Ahlam Majed on 20/10/2024.
+//
+
+import Testing
+@testable import Plant
+
+struct PlantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
